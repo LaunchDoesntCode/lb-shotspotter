aab6ffbaf84d49473a0d9fa894c116ac09ac7a92
launchmineous <launchogu@protonmail.com>
2025-03-12 14:39:35 -0500

    Rework

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Launch'
-description 'Fiveous Various Utilities'
-version '1.2.0'
-
-client_script "client.lua"
-shared_script "shared/**/*"
---
+++ lb-shotspotter/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Launch'
+description 'LB-Tablet dispatch for gun shots'
+version '1.2.0'
+
+client_script "client.lua"
+shared_script "shared/**/*"
+
+dependencies {
+  "lb-tablet",
+}
