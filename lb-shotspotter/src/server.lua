d365dde1c1eb1fd03731687bc094a168fe154481
Launch <71858690+LaunchDoesntCode@users.noreply.github.com>
2025-04-01 10:24:48 -0500

    Update server.lua
    added config options

--- lb-shotspotter/src/server.lua
+++ lb-shotspotter/src/server.lua
@@ -5,7 +5,7 @@ RegisterNetEvent("lb-shotspot:gunshotdispatch", function(street1, street2, x, y,
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
 
-    local gender = (Player.PlayerData.gender == 0) and "Male" or "Female"
+    local gender = (Player.PlayerData.gender == 0) and "Female" or "Male"
     local jobName = Player.PlayerData.job.name
 
     if Config.blacklistedJobs[jobName] then return end
@@ -21,27 +21,26 @@ RegisterNetEvent("lb-shotspot:gunshotdispatch", function(street1, street2, x, y,
         local policeDispatch = {
             priority    = 'high',
             code        = '10-04',
-            title       = isInVehicle and 'Shots fired from a vehicle' or 'Shots fired',
+            title = (Config.vehiclealerts and isInVehicle) and 'Shots fired from a vehicle' or 'Shots fired',
             description = ('Gunfire reported near %s'):format(locationLabel),
             location    = { label = locationLabel, coords = { x = x, y = y, z = z } },
             time        = 300,
-            job         = 'police',
+            job         = Config.policejob,
             fields      = {
                 { icon = 'fa-solid fa-gun', label = 'Weapon Class', value = weaponClass },
                 { icon = 'fa-solid fa-venus-mars', label = 'Gender', value = gender }
             }
         }
-
-        if isInVehicle then
-            --table.insert(policeDispatch.fields, { icon = 'fa-solid fa-car', label = 'License Plate', value = vehiclePlate })
-            table.insert(policeDispatch.fields, { icon = 'fa-solid fa-car', label = 'Type', value = vehicleType })
-            table.insert(policeDispatch.fields, { icon = 'fa-solid fa-car', label = 'Color', value = vehicleColor })
-        end
+	if Config.vehiclealerts and isInVehicle then
+	    --table.insert(policeDispatch.fields, { icon = 'fa-solid fa-car', label = 'License Plate', value = vehiclePlate })
+	    table.insert(policeDispatch.fields, { icon = 'fa-solid fa-car', label = 'Type', value = vehicleType })
+	    table.insert(policeDispatch.fields, { icon = 'fa-solid fa-car', label = 'Color', value = vehicleColor })
+	end
 
         exports["lb-tablet"]:AddDispatch(policeDispatch)
-		if Config.ambulancedispatch
-			policeDispatch.job = 'ambulance'
-			exports["lb-tablet"]:AddDispatch(policeDispatch)
-		end
+	if Config.ambulancedispatch
+		policeDispatch.job = Config.ambulancejob
+		exports["lb-tablet"]:AddDispatch(policeDispatch)
+	end
     end)
 end)
