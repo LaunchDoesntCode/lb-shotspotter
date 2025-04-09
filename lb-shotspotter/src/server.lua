c5c52be4e8efc28025e730f0a890562b58d4a0d7
Launch <71858690+LaunchDoesntCode@users.noreply.github.com>
2025-04-09 15:04:00 -0500

    Update server.lua

--- lb-shotspotter/src/server.lua
+++ lb-shotspotter/src/server.lua
@@ -19,7 +19,51 @@ CreateThread(function()
     end
 end)
 
+RegisterNetEvent("lb-shotspot:fightdispatch", function(street1, street2, x, y, z)
+    local src = source
+    local Player, jobName, gender
 
+    if useESX then
+        Player = Framework.GetPlayerFromId(src)
+        if not Player then return end
+        jobName = Player.getJob().name
+        gender = (Player.get("sex") == "f") and "Female" or "Male"
+    else
+        Player = Framework.Functions.GetPlayer(src)
+        if not Player then return end
+        jobName = Player.PlayerData.job.name
+        gender = (Player.PlayerData.gender == 0) and "Female" or "Male"
+    end
+
+    if Config.blacklistedJobs[jobName] then return end
+
+    local locationLabel = street1
+    if (street2 ~= nil and street2 ~= "") then
+        locationLabel = locationLabel .. " / " .. street2
+    end
+
+    Citizen.CreateThread(function()
+        Citizen.Wait(Config.dispatch_delay)
+        local dispatchData = {
+            priority    = 'medium',
+            code        = '10-10',
+            title       = 'Fighting Reported',
+            description = ('Physical altercation reported near %s'):format(locationLabel),
+            location    = { label = locationLabel, coords = { x = x, y = y, z = z } },
+            time        = 300,
+            fields      = {
+                { icon = 'fa-solid fa-fist-raised', label = 'Type', value = "Fist Fight" },
+                { icon = 'fa-solid fa-venus-mars', label = 'Gender', value = gender }
+            }
+        }
+
+        for _, job in ipairs(Config.dispatchJobs) do
+            local copy = dispatchData
+            copy.job = job
+            exports["lb-tablet"]:AddDispatch(copy)
+        end
+    end)
+end)
 
 RegisterNetEvent("lb-shotspot:gunshotdispatch", function(street1, street2, x, y, z, weaponClass, isInVehicle, vehiclePlate, vehicleType, vehicleColor, vehicleClass)
     local src = source
